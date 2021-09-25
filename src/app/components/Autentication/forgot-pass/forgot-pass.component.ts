136a246b036251ee62cfa71a51111c29e24bff62
Allen Vargas <allenbfox@gmail.com>
2021-09-24 20:11:51 -0400

    Solve problem with number decimal

--- src/app/components/Autentication/forgot-pass/forgot-pass.component.ts
+++ src/app/components/Autentication/forgot-pass/forgot-pass.component.ts
@@ -137,6 +137,8 @@ export class ForgotPassComponent implements OnInit {
     const crypto = window.crypto;
     var array = new Uint32Array(1);
     crypto.getRandomValues(array); // Compliant for security-sensitive use cases
-    return array[0];
+    var num = array[0];
+    var numStringFloat = '0.' + num.toString();
+    return parseFloat(numStringFloat);
   }
 }
